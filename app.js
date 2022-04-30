0f7720596bdc2c1fee46b9413d4cd717c3cc08a5
Amber Butt <amber96633@gmail.com>
2022-04-30 09:19:23 -0400

    made changes

---
+++ app.js
@@ -0,0 +1,20 @@
+let path = document.querySelector("#name");
+
+let pathLength = path.getTotalLength();
+
+path.style.strokeDasharray = pathLength + ' ' + pathLength;
+
+path.style.strokeDashoffset = pathLength;
+
+window.addEventListener("scroll", function (e) {
+
+    // What % down is it? 
+    var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+
+    // Length to offset the dashes
+    var drawLength = pathLength * scrollPercentage;
+
+    // Draw in reverse
+    path.style.strokeDashoffset = pathLength - drawLength;
+
+});
